7093a6edd8b9458313170c6db05888f5a1485860
Victor Karamalis <vek2012@nyu.edu>
2025-02-11 01:40:18 -0500

    Update theodora.py
    fix

--- theodora.py
+++ theodora.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import os
 from PIL import Image
@@ -6,12 +5,13 @@ from PIL import Image
 # Set the title of the Streamlit app
 st.title('COMING SOON: THEODORA, THE AI-POWERED RISK MANAGEMENT PLATFORM')
 
-
-
-
 # Specify the image file
-image_path = '/THEODORA_SYM.jpg'
+image_path = 'THEODORA_SYM.jpg'
 
-# Open and display the image file
-image = Image.open(image_path)
-st.image(image, caption='Specified Image', use_column_width=True)
+# Check if the image file exists
+if os.path.exists(image_path):
+    # Open and display the image file
+    image = Image.open(image_path)
+    st.image(image, caption='Specified Image', use_column_width=True)
+else:
+    st.error('Error: Image file not found.')
