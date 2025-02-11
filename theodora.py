7204615997a2536e142114db192fe2e6dbba054f
Victor Karamalis <vek2012@nyu.edu>
2025-02-11 01:44:39 -0500

    Update theodora.py

--- theodora.py
+++ theodora.py
@@ -2,10 +2,9 @@ import streamlit as st
 import os
 from PIL import Image
 
-# Set the title of the Streamlit app
-st.title('COMING SOON: /p
-THEODORA, /p
-THE AI-POWERED RISK MANAGEMENT PLATFORM')
+# Set the title with center justification using HTML and Markdown
+st.markdown("<h1 style='text-align: center;'>COMING SOON:<br>THEODORA, THE AI-POWERED RISK MANAGEMENT PLATFORM</h1>", unsafe_allow_html=True)
+
 
 # Specify the image file
 image_path = 'THEODORA_SYM.jpg'
