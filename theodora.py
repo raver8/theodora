df21bae873783cd2d82195a00ca946dd997686be
Victor Karamalis <vek2012@nyu.edu>
2025-02-11 01:51:28 -0500

    Update theodora.py

--- theodora.py
+++ theodora.py
@@ -3,9 +3,9 @@ import os
 from PIL import Image
 
 # Set the title with center justification using HTML and Markdown
-st.markdown("<h1 style='text-align: center;'>COMING SOON:<br>THEODORA, <br>THE AI-POWERED RISK MANAGEMENT PLATFORM</h1>", unsafe_allow_html=True)
-
-
+st.markdown("<h3 style='text-align: center;'>COMING SOON:</h3>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>THEODORA</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center;'>THE AI-POWERED RISK MANAGEMENT PLATFORM</h2>", unsafe_allow_html=True)
 # Specify the image file
 image_path = 'THEODORA_SYM.jpg'
 
