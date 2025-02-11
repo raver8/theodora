9c046fb7fb2a1cfae474718db421fb1ad2c8aa01
Victor Karamalis <vek2012@nyu.edu>
2025-02-11 01:47:51 -0500

    Update theodora.py
    solid

--- theodora.py
+++ theodora.py
@@ -3,7 +3,7 @@ import os
 from PIL import Image
 
 # Set the title with center justification using HTML and Markdown
-st.markdown("<h1 style='text-align: center;'>COMING SOON:<br>THEODORA, THE AI-POWERED RISK MANAGEMENT PLATFORM</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>COMING SOON:<br>THEODORA, <br>THE AI-POWERED RISK MANAGEMENT PLATFORM</h1>", unsafe_allow_html=True)
 
 
 # Specify the image file
@@ -13,6 +13,6 @@ image_path = 'THEODORA_SYM.jpg'
 if os.path.exists(image_path):
     # Open and display the image file
     image = Image.open(image_path)
-    st.image(image, caption='Specified Image', use_column_width=True)    
+    st.image(image, caption='_', use_column_width=True)    
 else:
     st.error('Error: Image file not found.')
